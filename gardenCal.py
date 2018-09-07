2aa45b76895f2c7ac982cbc4c625b924b3673633
Kirill Simin <kirill@kirillsimin.com>
2018-09-07 13:56:55 +0000

    refactor, fix links

--- gardenCal.py
+++ gardenCal.py
@@ -1,76 +1,52 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Mar  5 13:00:22 2016
-
-@author: Kirill
-"""
-
 from bs4 import BeautifulSoup
-from urllib.request import urlopen
-import re
-import csv
+import urllib
 
-def ks():
-    print('ks')
+
+def parsePage(url):
+    req = urllib.request.Request(url, headers={'User-Agent' : "Magic Browser"}) 
+    con = urllib.request.urlopen( req )
+    content = con.read()
+    return BeautifulSoup(content)
     
-def getVegs(url="http://www.clemson.edu/extension/hgic/plants/vegetables/crops/"):
-    url = urlopen(url)
-    content = url.read()
-    soup = BeautifulSoup(content)  
+def getVegLinks():
+    soup = parsePage("https://hgic.clemson.edu/all-factsheets/")
     links = soup.find_all("a")
+
     allVegs = []
     
     for link in links:
-        if 'HGIC 13' in str(link):
+        if 'HGIC 13' in link.contents[0]:
             allVegs.append(link['href'])
     
     return allVegs
-    
-def vegInfo(page="hgic1301.html"):
-    fullUrl = "http://www.clemson.edu/extension/hgic/"
-    fullUrl += "plants/vegetables/crops/"
-    fullUrl += page
-    webpage = urlopen(fullUrl)
-    content = webpage.read()    
-    soup = BeautifulSoup(content)
-    
-    title = soup.title.string
-    title = title.strip()
-    title = title.split(':',1)[0]
-    title = title[10:]
-    title = re.sub('\s+', '', title)
-    
-    data = []
-    data.append(title)
-    #print('##### &&&& **** ',title)
-    
+
+def getVegDates(soup, region):
+
     if(soup.find("tbody")):
         table = soup.find("tbody")        
         rows = table.find_all('tr')        
         for row in rows:
-            #print (row)
-            cols = row.findAll('td')
-            cols = [ele.text.strip() for ele in cols]
-            #print(cols)
-            #if 'Central' in cols:
-            data.append(cols)
+            dates = row.findAll('td')
+            dates = [date.text.strip() for date in dates]
+            if region in dates:
+                return dates
+
+def parseVeg(url, region='Central'):
+    soup = parsePage(url)
+    
+    title = soup.title.string.split('|')[0]
+    
+    data = []
+    data.append(title)
+    data.append(getVegDates(soup, region))    
 
     return data
 
-class Vegetable():
-     def __init__(self):
-        self._piedmont = list()
-        self._central = list()
-        self._coastal = list()
-
 
 if __name__ == "__main__":
-#==============================================================================
-#     print(vegInfo())
-#==============================================================================
 
-     allVegs = getVegs()
-     for veg in allVegs:
-         print()
-         print(vegInfo(veg))
+    allVegs = getVegLinks()
+    for veg in allVegs:
+        print(parseVeg(veg))
+
 
